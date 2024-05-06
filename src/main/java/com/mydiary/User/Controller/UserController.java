6228c9b128e124dbc918d9b506686e94f442d303
MeryemBenSalem <meryem.bensalem@insat.ucar.tn>
2024-05-06 23:47:59 +0100

    UI Update + Fixed Bugs

--- src/main/java/com/mydiary/User/Controller/UserController.java
+++ src/main/java/com/mydiary/User/Controller/UserController.java
@@ -1,5 +1,4 @@
 package com.mydiary.User.Controller;
-
 import com.mydiary.User.Model.User;
 import com.mydiary.User.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3002")
 public class UserController {
 
     @Autowired
